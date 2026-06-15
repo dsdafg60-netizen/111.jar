b16adddad31024f68eb77f60daf1f886ebbe2ee5
dsdafg60-netizen <dsdafg60@gmail.com>
2026-06-15 14:16:31 +0300

    Add items registry

---
+++ src/main/java/com/dsdafg/mod111/item/ModItems.java
@@ -0,0 +1,13 @@
+package com.dsdafg.mod111.item;
+
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.item.Item;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = 
+        DeferredRegister.create(ForgeRegistries.ITEMS, "mod111");
+
+    // Можно добавить специальные предметы позже
+}
