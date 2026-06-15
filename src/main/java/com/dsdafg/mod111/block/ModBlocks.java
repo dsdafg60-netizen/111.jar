ccfa4e9eadf44f4ec555ef0ac78c0d6ea2fb4974
dsdafg60-netizen <dsdafg60@gmail.com>
2026-06-15 14:16:23 +0300

    Add blocks registry

---
+++ src/main/java/com/dsdafg/mod111/block/ModBlocks.java
@@ -0,0 +1,12 @@
+package com.dsdafg.mod111.block;
+
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraft.world.level.block.Block;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS = 
+        DeferredRegister.create(ForgeRegistries.BLOCKS, "mod111");
+
+    // Можно добавить специальные блоки позже
+}
